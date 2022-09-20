7f11fb29f473077c19023e2b2d74bb7b954f123f
Luis Barni <luis.barni@gunidavi.edu.br>
2022-09-20 17:00:54 -0300

    criação do app

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+use App\Http\Controllers\CombustivelController;
+
+route::get('/', 'CombustivelController@index');
+
+route::get('/gasto', 'CombustivelController@gasto');
